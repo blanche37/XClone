81703c40297c17fe654690ffba0c56876417c1b7
jang.yunhwa@cloco.co.jp <jang.yunhwa@cloco.co.jp>
2023-08-01 12:58:36 +0900

    Create TimelineViewController
    add scrollView
    add tableView
    add ViewModel

---
+++ XClone/XClone/ViewModel/TimelineViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  TimelineViewModel.swift
+//  XClone
+//
+//  Created by yun on 2023/08/01.
+//
+
+import Combine
+import UIKit
+
+protocol TimelineViewModelProtocol {
+    
+}
+
+final class TimelineViewModel: TimelineViewModelProtocol {
+
+}
