68d178adaa3b9ed7cc059691762a6b8ea7988935
jang.yunhwa@cloco.co.jp <jang.yunhwa@cloco.co.jp>
2023-08-01 11:37:28 +0900

    first commit

---
+++ XClone/XClone/View/Container/ContainerViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  XClone
+//
+//  Created by yun on 2023/08/01.
+//
+
+import UIKit
+
+final class ContainerViewController: UITabBarController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
---
+++ XClone/XClone/View/Container/ContainerViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContainerViewModel.swift
+//  XClone
+//
+//  Created by yun on 2023/08/01.
+//
+
+import Foundation
