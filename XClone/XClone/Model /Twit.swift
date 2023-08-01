906656fedfe51ff630a081f114368e942a36b004
jang.yunhwa@cloco.co.jp <jang.yunhwa@cloco.co.jp>
2023-08-01 12:09:54 +0900

    Add json for testing and Add CodingKey

--- XClone/XClone/Model /Twit.swift
+++ XClone/XClone/Model /Twit.swift
@@ -10,7 +10,15 @@ import Foundation
 struct Twit: Codable {
     var nickName: String
     var mentionId: String
-    var profileImage: Data?
+    var profileImage: String?
     var content: String
-    var contentImage: Data?
+    var contentImage: String?
+    
+    private enum CodingKeys: String, CodingKey {
+        case nickName = "nick_name"
+        case mentionId = "mention_id"
+        case profileImage = "profile_image"
+        case content
+        case contentImage = "content_image"
+    }
 }
